da1b70f8635ea267f03b9262be8bb458dd29f936
Dang Trung Hieu <hieudt230899@gmail.com>
2020-09-04 22:02:13 +0700

    * Tabbar Controller * Add icon tabbar * Group folder

---
+++ Instagram_Clone/Instagram_Clone/Controllers/Other/Onboarding/RegistrantionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrantionViewController.swift
+//  Instagram_Clone
+//
+//  Created by Dang Trung Hieu on 9/4/20.
+//  Copyright © 2020 Dang Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+class RegistrantionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
--- Instagram_Clone/Instagram_Clone/Resource/AppDelegate.swift
+++ Instagram_Clone/Instagram_Clone/Resource/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+
+        FirebaseApp.configure()
         return true
     }
 
